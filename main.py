85bbfbba14eabe6a1c5f37ecc0da18cc47a73742
odara-rapheal <odararapheal@gmail.com>
2025-06-07 15:20:49 +0100

    changed llama model to 3.2 3B Param

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_middleware(
 )
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
-MODEL_NAME = "llama3:2.1b"
+MODEL_NAME = "llama3.2"
 
 def stream_ollama(prompt: str):
     payload = {
